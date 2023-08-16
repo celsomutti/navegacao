a48395bbb86c73cdb79da4a2300c92ada1d7a8ee
unknown <celsomutti@gmail.com>
2023-08-16 17:28:27 -0300

    Primeiro Commit

---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+//import './Home.css'
+const Home = props => (
+    <div className='Home'>
+        <h1>Início</h1>
+        <p>Bem vindo!</p>
+    </div>
+)
+
+export default Home
